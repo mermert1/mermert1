3fa88e8eda7e31aa5b9caffe0a17621685f88e88
AtlanticFrameworks <atlanticframeworks@gmail.com>
2026-02-04 12:19:56 +0100

    Fix invalid Mermaid arrow syntax in state.ts

--- src/lib/util/state.ts
+++ src/lib/util/state.ts
@@ -17,11 +17,11 @@ import { errorDebug, formatJSON, MCBaseURL } from './util';
 
 export const defaultState: State = {
   code: `flowchart TD
-A[Christmas]-- >| Get money | B(Go shopping)
-B-- > C{Let me think }
-C-- >| One | D[Laptop]
-C-- >| Two | E[iPhone]
-C-- >| Three | F[fa: fa - car Car]
+A[Christmas] -->| Get money | B(Go shopping)
+B --> C{Let me think }
+C -->| One | D[Laptop]
+C -->| Two | E[iPhone]
+C -->| Three | F[fa: fa - car Car]
 `,
   grid: true,
   mermaid: formatJSON({
@@ -35,13 +35,13 @@ C-- >| Three | F[fa: fa - car Car]
 
 const urlParseFailedState = `flowchart TD
 A[Loading URL failed.We can try to figure out why.]-->| Decode JSON | B(Please check the console to see the JSON and error details.)
-B-- > C{Is the JSON correct ?}
-C-- >| Yes | D(Please Click here to Raise an issue in github.< br /> Including the broken link in the issue < br /> will speed up the fix.)
-C-- >| No | E{Did someone < br /> send you this link ?}
-E-- >| Yes | F[Ask them to send < br /> you the complete link]
-E-- >| No | G{Did you copy < br /> the complete URL ?}
-G-- > | Yes | D
-G-- > | "No :(" | H(Try using the Timeline tab in History < br /> from same browser you used to create the diagram.)
+B --> C{Is the JSON correct ?}
+C -->| Yes | D(Please Click here to Raise an issue in github.< br /> Including the broken link in the issue < br /> will speed up the fix.)
+C -->| No | E{Did someone < br /> send you this link ?}
+E -->| Yes | F[Ask them to send < br /> you the complete link]
+E -->| No | G{Did you copy < br /> the complete URL ?}
+G --> | Yes | D
+G --> | "No :(" | H(Try using the Timeline tab in History < br /> from same browser you used to create the diagram.)
     click D href "https://github.com/mermaid-js/mermaid-live-editor/issues/new?assignees=&labels=bug&template=bug_report.md&title=Broken%20link" "Raise issue"`;
 
 // inputStateStore handles all updates and is shared externally when exporting via URL, History, etc.
